2f9c7529a1f64390a3e036da20e5d38df29aee5d
Frédéric Mangano <fmang@mg0.fr>
2024-01-06 22:07:13 +0900

    Convertir l’image en noir et blanc

--- kakeibo.cc
+++ kakeibo.cc
@@ -1,7 +1,11 @@
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
 #include <iostream>
+
+static const int threshold_block_size = 15;
+static const double threshold_offset = -20;
 
 int main(int argc, char** argv)
 {
@@ -9,10 +13,14 @@ int main(int argc, char** argv)
 		std::cerr << "Usage: " << argv[0] << " FILE" << std::endl;
 		return 1;
 	}
-
 	std::string image_path = argv[1];
-	cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
-	cv::imshow("", img);
+
+	cv::Mat gray = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+	cv::Mat binary;
+	cv::adaptiveThreshold(~gray, binary, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY,
+	                      threshold_block_size, threshold_offset);
+
+	cv::imshow("", binary);
 	cv::waitKey(0);
 	return 0;
 }
