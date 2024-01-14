d3610eab9e63962beef14c099e82335474686182
Frédéric Mangano <fmang@mg0.fr>
2024-01-14 21:27:03 +0900

    Détection des countours sur les tickets

--- scanner.cc
+++ scanner.cc
@@ -19,8 +19,24 @@ int main(int argc, char** argv)
 
 	const char* image_path = argv[1];
 	cv::Mat source = cv::imread(image_path, cv::IMREAD_COLOR);
-	cv::imshow("Source", source);
 
+	// Extrait le rouge pour rendre les tampons moins visibles. Les tickets
+	// sont monochromes, donc tous les canaux sont plus ou moins égaux.
+	cv::Mat red;
+	cv::extractChannel(source, red, 2);
+	cv::bitwise_not(red, red);
+	cv::adaptiveThreshold(red, red, 255, cv::THRESH_BINARY, cv::ADAPTIVE_THRESH_MEAN_C, 75, -50);
+
+	cv::Mat drawing = source.clone();
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(red, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	for (auto& contour : contours) {
+		cv::Rect box = cv::boundingRect(contour);
+		cv::rectangle(drawing, box.tl(), box.br(), cv::Scalar(0, 0, 255), 2);
+	}
+
+	cv::imshow("Red", red);
+	cv::imshow("Countours", drawing);
 	cv::waitKey(0);
 	return 0;
 }
