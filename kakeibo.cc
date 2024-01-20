7705c11cc98299966f824a0a2cf2aeda1ab416c2
Frédéric Mangano <fmang@mg0.fr>
2024-01-20 22:07:15 +0900

    Extraire les reçus dans un dossier spécifique

--- kakeibo.cc
+++ kakeibo.cc
@@ -2,23 +2,26 @@
 
 #include <opencv2/imgcodecs.hpp>
 
-#include <iostream>
+#include <cstdio>
+#include <filesystem>
 
 int main(int argc, char** argv)
 {
 	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " FILE..." << std::endl;
+		std::fprintf(stderr, "Usage: %s FILE…\n", argv[0]);
 		return 2;
 	}
 
 	int extracted_count = 0;
+	std::filesystem::create_directories("extracted");
+
 	for (int argi = 1; argi < argc; ++argi) {
 		const char* image_path = argv[argi];
 		cv::Mat source = cv::imread(image_path, cv::IMREAD_COLOR);
 		
 		for (auto receipt : extract_receipts(source)) {
-			std::string output_file = "extracted-" + std::to_string(++extracted_count) + ".jpg";
-			std::cout << output_file << std::endl;
+			std::string output_file = "extracted/" + std::to_string(++extracted_count) + ".jpg";
+			std::puts(output_file.c_str());
 			cv::imwrite(output_file, receipt);
 		}
 	}
