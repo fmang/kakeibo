301e77746de55feebb8c6b8dd809820fc3591db8
Frédéric Mangano <fmang@mg0.fr>
2024-02-07 17:31:03 +0900

    Détecter les lettres par contour

--- src/detector.cc
+++ src/detector.cc
@@ -11,79 +11,88 @@
 #include <opencv2/imgproc.hpp>
 
 /**
- * Supprime tous les pixels noirs autour d’une image.
+ * Représente une ligne de texte. box est la bounding box sur l’image d’entrée.
+ * letters est une liste de bounding boxes pour chaque lettre de la ligne.
  */
-static cv::Mat trim(cv::Mat image)
+struct text_line {
+	cv::Rect box;
+	std::vector<cv::Rect> letters;
+};
+
+/**
+ * Reçoit une image binaire et la position de la ligne à extraire.
+ * Extrait les lettres de la ligne et construit l’objet text_line.
+ */
+static text_line extract_text_line(cv::Mat binary, cv::Rect line_box)
 {
-	cv::Mat binary;
-	cv::cvtColor(image, binary, cv::COLOR_BGR2GRAY);
-	cv::threshold(binary, binary, 128, 255, cv::THRESH_BINARY);
-	cv::bitwise_not(binary, binary);
+	cv::Mat extract = binary(line_box);
 
-	std::vector<cv::Point> whites;
-	cv::findNonZero(binary, whites);
-	cv::Rect box = cv::boundingRect(whites);
-	return image(box);
-}
+	// La dilatation verticale permet de rassembler les contours d’une même lettre.
+	cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 5));
+	cv::dilate(extract, extract, element);
 
-static void extract_letter(cv::Mat fragment)
-{
-	cv::Mat letter = trim(fragment);
-	if (letter.empty())
-		return;
+	std::vector<cv::Rect> letters;
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(extract, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	for (auto& contour : contours) {
+		cv::Rect letter = cv::boundingRect(contour);
+		letter.x += line_box.x;
+		letter.y += line_box.y;
+		letters.push_back(letter);
+	}
 
-	show("Letter", letter);
+	// TODO: Trier les lignes par x.
+
+	return text_line { line_box, letters };
 }
 
 /**
- * Reçoit un fragment d’image contenant par exemple « 合計 », et extrait chaque
- * lettre individuelle. On suppose que le fragment contient exactement 2
- * lettres.
+ * Reçoit une image binaire et détecte les lignes de texte.
+ * Renvoie la liste des text_line trouvés.
  */
-static void extract_letters(cv::Mat fragment)
+static std::vector<text_line> extract_text_lines(cv::Mat binary)
 {
-	int half = fragment.cols / 2;
-	cv::Mat left = fragment(cv::Range::all(), cv::Range(0, half));
-	cv::Mat right = fragment(cv::Range::all(), cv::Range(half, fragment.cols));
-	extract_letter(left);
-	extract_letter(right);
+	// La dilatation horizontale permet de rassembler les lignes dans un même contour.
+	cv::Mat dilated;
+	cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 1));
+	cv::dilate(binary, dilated, element);
+
+	std::vector<text_line> lines;
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(dilated, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	for (auto& contour : contours) {
+		cv::Rect box = cv::boundingRect(contour);
+		lines.push_back(extract_text_line(binary, box));
+	}
+
+	return lines;
+}
+
+/**
+ * Affiche pour le debug l’image source avec les lignes encadrées en rouge et
+ * les lettre en bleu.
+ */
+static void show_text_lines(cv::Mat source, const std::vector<text_line> lines)
+{
+	cv::Mat drawing = source.clone();
+	for (const text_line& line : lines) {
+		for (const cv::Rect& letter : line.letters)
+			cv::rectangle(drawing, letter.tl(), letter.br(), cv::Scalar(255, 0, 0), 1);
+		cv::rectangle(drawing, line.box.tl(), line.box.br(), cv::Scalar(0, 0, 255), 2);
+	}
+	show("Text lines", drawing);
 }
 
 void scan_receipt(cv::Mat source)
 {
 	// Extrait le rouge pour rendre les tampons moins visibles. Les tickets
 	// sont monochromes, donc tous les canaux sont plus ou moins égaux.
-	cv::Mat red;
-	cv::extractChannel(source, red, 2);
-	cv::bitwise_not(red, red);
-	cv::adaptiveThreshold(red, red, 255, cv::THRESH_BINARY, cv::ADAPTIVE_THRESH_MEAN_C, 75, -50);
+	cv::Mat binary;
+	cv::extractChannel(source, binary, 2); // Canal rouge.
+	cv::bitwise_not(binary, binary); // Blanc sur noir.
+	cv::adaptiveThreshold(binary, binary, 255, cv::THRESH_BINARY, cv::ADAPTIVE_THRESH_MEAN_C, 75, -50);
 
-	// La dilatation horizontale permet de faire en sorte que les mots se
-	// collent. La détection de contour nous donnera alors un contour par
-	// mot, plutôt qu’un par lettre. Ça permet aussi d’unifier les kanjis.
-	cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 1));
-	cv::dilate(red, red, element);
-	show("Red", red);
-
-	cv::Mat drawing = debug ? source.clone() : cv::Mat();
-
-	std::vector<std::vector<cv::Point>> contours;
-	cv::findContours(red, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-	for (auto& contour : contours) {
-		cv::Rect box = cv::boundingRect(contour);
-		double aspect = static_cast<double>(box.width) / box.height;
-		bool promising =
-			box.width > 40 && box.height > 20 &&
-			box.width < 150 && box.height < 100 &&
-			aspect > 2 && aspect < 6 &&
-			box.x + box.width < red.cols / 2;
-
-		if (promising)
-			extract_letters(source(box));
-
-		if (debug)
-			cv::rectangle(drawing, box.tl(), box.br(), promising ? cv::Scalar(0, 255, 0) : cv::Scalar(0, 0, 255), 2);
-	}
+	std::vector<text_line> lines = extract_text_lines(binary);
 	if (debug)
-		show("Countours", drawing);
+		show_text_lines(source, lines);
 }
