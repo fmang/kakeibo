d3c666cba54ba98d852bf654a6915693ad0477d8
Frédéric Mangano <fmang@mg0.fr>
2024-03-23 18:41:24 +0900

    Dynamiser le formulaire

--- static/ui.js
+++ static/ui.js
@@ -14,3 +14,36 @@ takePictureButton.addEventListener("click", (event) => {
 	pictureSelector.setAttribute("capture", "environment");
 	pictureSelector.showPicker();
 });
+
+function today() {
+	return new Date().toISOString().split("T")[0];
+}
+
+const entryForm = document.forms["entry"];
+const dateField = entryForm.elements["date"];
+const amountField = entryForm.elements["amount"];
+const remarkField = entryForm.elements["remark"];
+
+entryForm.addEventListener("reset", (event) => {
+	dateField.defaultValue = today();
+	amountField.focus();
+});
+entryForm.reset();
+
+let lastFocus = Date.now();
+document.addEventListener("focus", (event) => {
+	let now = Date.now();
+	if (now - lastFocus > 3600 && !amountField.value && !remarkField.value)
+		entryForm.reset();
+	lastFocus = now;
+});
+
+const setTodayButton = document.getElementById("set-today-button");
+setTodayButton.addEventListener("click", (event) => {
+	dateField.value = today();
+});
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", (event) => {
+	event.target.closest("form").reset();
+});
